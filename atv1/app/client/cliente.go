b8e581290ceb9594ac3850f67610a0de35f04437
Amanda Moraes <amandascm23@gmail.com>
2024-10-29 21:30:19 -0300

    chore: extract only used modules to new atv1 folder

---
+++ atv1/app/client/cliente.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	calculadoraproxy "test/atv1/distribution/proxies/calculadora"
+	namingproxy "test/atv1/services/naming/proxy"
+	"test/shared"
+	"time"
+)
+
+func main() {
+	Cliente()
+}
+
+func Cliente() {
+
+	ClientePerf()
+	os.Exit(0)
+
+	// Obtain proxies
+	naming := namingproxy.New(shared.LocalHost, shared.NamingPort)
+	calc := calculadoraproxy.New(naming.Find("Calculadora"))
+
+	// Chamada remota a Calculadora
+	fmt.Println(calc.Som(1, 2))
+}
+
+func ClientePerf() {
+	naming := namingproxy.New(shared.LocalHost, shared.NamingPort)
+	calc := calculadoraproxy.New(naming.Find("Calculadora"))
+
+	for i := 0; i < shared.StatisticSample; i++ {
+		t1 := time.Now()
+		for j := 0; j < shared.SampleSize; j++ {
+			calc.Som(1, 2)
+		}
+		fmt.Println(i, ";", time.Now().Sub(t1).Milliseconds())
+		time.Sleep(100 * time.Millisecond)
+	}
+	fmt.Println("Experiemnt finalised...")
+}
---
+++ atv1/distribution/pool/calculadorapool.go
@@ -0,0 +1,31 @@
+package calculadorapool
+
+import (
+	"fmt"
+	"test/atv1/app/businesses/calculadora"
+)
+
+type CalculadoraPool struct {
+	pool chan *calculadora.Calculadora
+}
+
+func NewObjectPool(size int) *CalculadoraPool {
+	pool := make(chan *calculadora.Calculadora, size)
+	for i := range size {
+		pool <- &calculadora.Calculadora{ID: i}
+	}
+	return &CalculadoraPool{
+		pool: pool,
+	}
+}
+
+func (calcPool *CalculadoraPool) Get() *calculadora.Calculadora {
+	obj := <-calcPool.pool
+	fmt.Printf("Got object available at channel with ID: %d\n", obj.ID)
+	return obj
+}
+
+func (calcPool *CalculadoraPool) Put(obj *calculadora.Calculadora) {
+	fmt.Printf("Returned to pool object with ID: %d\n", obj.ID)
+	calcPool.pool <- obj
+}
